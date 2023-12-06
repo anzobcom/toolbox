d95417cddf3000a3a8178a619ae201edf92c3346
Lucky Coder <47534548+Muxsin@users.noreply.github.com>
2023-12-06 20:42:58 +0700

    Wrote script for inserting Parex products list (#2)
    * Closes #1 : Created classes Database and DbInsertCommand
    
    * Closes #1: Reading csv file
    
    * Closes #1: Wrote code to insert data into db from csv file
    
    * closes #1: resolved requsted changes
    
    * Closes #1: Resolved requested changes
    
    * Resolved requsted changes

---
+++ src/Database.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Anzob\ToolboxApp;
+
+use PDO;
+use PDOStatement;
+
+class Database
+{
+    public PDO $connection;
+    public PDOStatement|bool $statement;
+
+    public function __construct(DatabaseConfig $config)
+    {
+        $dsn = 'mysql:host=' . $config->getHost()
+            .';port=' . $config->getPort()
+            . ';dbname=' . $config->getDbname()
+            . ';charset=' . $config->getCharset();
+
+        $this->connection = new PDO($dsn, $config->getUsername(), $config->getPassword(), [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
+    }
+
+    public function query($query, $params = []): self
+    {
+        $this->statement = $this->connection->prepare($query);
+
+        $this->statement->execute($params);
+
+        return $this;
+    }
+
+    public function get(): false|array
+    {
+        return $this->statement->fetchAll();
+    }
+}
---
+++ src/ProductsImportCommand.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Anzob\ToolboxApp;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ProductsImportCommand extends Command
+{
+    private const ENGLISH_LANGUAGE = 1;
+    private const ENG_DESC_COL = 11;
+    private const RUSSIAN_LANGUAGE = 2;
+    private const RU_DESC_COL = 12;
+
+    protected function configure(): void
+    {
+        $this
+            ->setName('import-products')
+            ->setDescription('Import products.')
+            ->setHelp('This command allows you to import products.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $config = new DatabaseConfig('flamingo');
+        $database = new Database($config);
+
+        $csvFile = file('parex-product-list.csv');
+        $data = [];
+
+        foreach ($csvFile as $line) {
+            $data[] = str_getcsv($line);
+        }
+
+        for($i = 0; $i < count($data); $i++) {
+            if ($data[$i][self::RU_DESC_COL] !== '') {
+                $database->query(
+                    'INSERT INTO oc_product_description (
+                            language_id, 
+                            name, 
+                            description, 
+                            meta_title) 
+                    VALUES (
+                            :language_id, 
+                            :name, 
+                            :description, 
+                            :meta_title
+                    )',
+                    [
+                        'language_id' => self::RUSSIAN_LANGUAGE,
+                        'name' => $data[$i][self::RU_DESC_COL],
+                        'description' => $data[$i][self::RU_DESC_COL],
+                        'meta_title' => $data[$i][self::RU_DESC_COL]
+                    ]
+                );
+            }
+        }
+
+        return Command::SUCCESS;
+    }
+}
